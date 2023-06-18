5e44bb45b1005789775e968180030e5d708bd17b
Roman Pisarevsky <prp23@mail.ru>
2023-06-18 22:17:18 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,54 @@
+from telegram import *
+from telegram.ext import *
+import sqlite3
+from set import TOKEN_BOT
+
+
+# Создание объекта Бот
+application = Application.builder().token(TOKEN_BOT).build()
+
+conn = sqlite3.connect('mydb.db')
+cursor = conn.cursor()
+users = cursor.execute("SELECT user_name FROM USERS").fetchall()
+names = [row[0] for row in users]
+
+CHEK_PAL = range(1)
+
+# Функция старта
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+     for i in names:
+          if i == update.effective_user.username:    
+               await update.message.reply_text(f'Привет, {update.effective_user.username}! Напиши слово, и я проверю его на палиндром!') 
+               return CHEK_PAL
+          else:
+               await update.message.reply_text("Извини, но тебя нет в базе данных.")
+               return ConversationHandler.END
+          
+
+# Проверка на палиндром 
+async def chek_pal(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+     if update.message.text.replace(' ','').lower() == ''.join(reversed(update.message.text.replace(' ','').lower())):
+          await update.message.reply_photo("https://imageup.ru/img280/4376986/yes.png")
+     else:
+          await update.message.reply_photo("https://imageup.ru/img76/4377006/no.jpg")
+
+
+# Выход из диалога
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+     await update.message.reply_text("Спасибо, приходите еще!")
+     return ConversationHandler.END
+
+ 
+# Регистрация обработчика 
+handler = ConversationHandler(
+     entry_points=[CommandHandler('start', start)],
+     states={
+          CHEK_PAL: [MessageHandler(filters.TEXT & ~filters.COMMAND, chek_pal)],
+     },
+     fallbacks=[CommandHandler('cancel', cancel)]
+     ) 
+
+application.add_handler(handler)
+
+# Запуск бота
+application.run_polling()
